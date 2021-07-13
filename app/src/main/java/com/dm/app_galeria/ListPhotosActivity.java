7405c078290bcfee0b5ec757460a56ea8d5912da
marciosamuel <marciosamuel12@outlook.com>
2021-07-13 13:07:22 -0300

    página de listagem e página de upload prontas

---
+++ app/src/main/java/com/dm/app_galeria/ListPhotosActivity.java
@@ -0,0 +1,14 @@
+package com.dm.app_galeria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListPhotosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_photos);
+    }
+}
