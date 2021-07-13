d85ce826e8f88e1ca377c50b9d0a13a9742d0374
marciosamuel <marciosamuel12@outlook.com>
2021-07-13 16:33:42 -0300

    correção de dark mode e cor do card

--- app/src/main/java/com/dm/app_galeria/ListPhotosActivity.java
+++ app/src/main/java/com/dm/app_galeria/ListPhotosActivity.java
@@ -1,6 +1,7 @@
 package com.dm.app_galeria;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -19,6 +20,7 @@ public class ListPhotosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_photos);
 
